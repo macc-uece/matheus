9632012f4b8d521ed7758a0888fcd005cc6184cd
Matheus Paixao <mhepaixao@gmail.com>
2018-10-05 09:28:10 -0300

    Setting up git

---
+++ travis_test/src/main/java/frameworks/travis_test/App.java
@@ -0,0 +1,17 @@
+package frameworks.travis_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public int soma(int n1, int n2) {
+    	return n1 + n2;
+    }
+}
